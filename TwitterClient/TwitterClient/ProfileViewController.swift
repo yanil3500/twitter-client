27fd10b922a1b149dd472297fb2e34751071b937
yanil3500 <yanil3500@gmail.com>
2017-03-22 19:25:26 -0700

    Adds a viewController that displays a user's profile information via storyboard segue

--- TwitterClient/TwitterClient/ProfileViewController.swift
+++ TwitterClient/TwitterClient/ProfileViewController.swift
@@ -12,11 +12,20 @@ class ProfileViewController: UIViewController {
     
     var userProfile : User!
     
+    @IBOutlet weak var twitterUserName: UILabel!
+    @IBOutlet weak var userLocation: UILabel!
+    @IBOutlet weak var screenName: UILabel!
+    @IBOutlet weak var profileDescription: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         print("Inside of ProfileViewController: \(userProfile.profileDescription)")
         
+        
+        twitterUserName.text = "Username: \(userProfile.userName)"
+        userLocation.text = "Location: \(userProfile.location)"
+        screenName.text = "Screenname: \(userProfile.screenName)"
+        profileDescription.text = "Description: \(userProfile.profileDescription)"
         
     }
 
