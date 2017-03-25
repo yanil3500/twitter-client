e4735345e7d46c4febd1dea03e9c24fec87696b9
yanil3500 <yanil3500@gmail.com>
2017-03-24 17:23:23 -0700

    Adds UIImageView for UserTimelineViewController

--- TwitterClient/TwitterClient/UserTimelineViewController.swift
+++ TwitterClient/TwitterClient/UserTimelineViewController.swift
@@ -8,32 +8,54 @@
 
 import UIKit
 
-class UserTimelineViewController: UIViewController {
+class UserTimelineViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var userTimeLineImage: UIImageView!
+    @IBOutlet weak var userTimeLineTableView: UITableView!
+    
     var userProfile : User!
     
-    @IBOutlet weak var userTimeLineTableView: UITableView!
+    var userTweet = [Tweet](){
+        didSet {
+            self.userTimeLineTableView.reloadData()
+        }
+    }
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.navigationItem.title = "User Timeline"
         print("Inside of userTimeLineViewController: \(userProfile.screenName)")
+        
+        //get user profile pic 
+        
+        UIImage.fetchImageWith(userProfile.profilePißcURL) { (image) in
+            self.userTimeLineImage.image = image
+        }
+        
+        //Our UserTimeLineViewController
+        self.userTimeLineTableView.dataSource = self
+        self.userTimeLineTableView.delegate = self
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //Register tweet nib for reuse; Tell the tableview to use the tweet nib for its cell
+        
+        self.userTimeLineTableView.estimatedRowHeight = 50
+        self.userTimeLineTableView.rowHeight = UITableViewAutomaticDimension
+        
+        let tweetNib = UINib(nibName: "TweetNibCell", bundle: nil)
+        
+        self.userTimeLineTableView.register(tweetNib, forCellReuseIdentifier: TweetNibCell.identifier)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 }
